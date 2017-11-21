9799741c85ace1b50d9ddd3d3c92c4ba57ff1866
mysticPrg <mysticPrg@github.com>
2017-11-22 00:43:40 +0900

    change design

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,30 @@
 import * as React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
-// import SearchBox from './SearchBox';
 import SearchContainer from '../Containers/SearchContainer';
+const bg = require('../Resources/header_bg.jpg');
+
+const styles = StyleSheet.create({
+	header: {
+		width: '100%',
+		minHeight: '300px',
+		backgroundColor: '#ffc800',
+		background: `url(${bg})`,
+		backgroundSize: 'cover',
+		backgroundPositionY: 'center',
+		display: 'flex',
+		flexDirection: 'column',
+	},
+
+	title: {
+		color: '#FFFFFF',
+		fontSize: '50pt',
+		fontWeight: 900,
+		margin: 'auto',
+		outline: '1px solid #FFFFFF',
+		padding: '25px',
+	},
+});
 
 interface Props {
 	readonly title: string;
@@ -17,10 +40,8 @@ class Header extends React.Component<Props> {
 	
 	render() {
 		return (
-			<div id="header">
-				<div>
-					Header: {this.props.title}
-				</div>
+			<div id="header" className={css(styles.header)}>
+				<div className={css(styles.title)}>{this.props.title}</div>
 				<SearchContainer />
 			</div>
 		);
--- src/Components/SeminarCard.tsx
+++ src/Components/SeminarCard.tsx
@@ -18,7 +18,9 @@ interface State {
 
 const Styles = style.create({
 	base: {
-		outline: '1px solid black',
+		// outline: '1px solid black',
+		backgroundColor: '#FFFFFF',
+		boxShadow: '0 5px 15px 0px #0000002b',
 		width: '300px',
 		height: '200px',
 		margin: '10px',
